c91f184079b69dba70ca35cb1ecaeb5da69efafb
Alex Lutay <1928266+taurus-forever@users.noreply.github.com>
2023-03-07 14:25:43 +0100

    Add simple integration test (to test the bundle on PR) (#26)
    * Add simple integration test (to test the bundle on PR)
    
    * Add integration test to GH CI/CD
    
    (cherry picked from commit 287d58867eddaf5b70784b0b7033097dc85c7eb2)
    
    * Fix lint warning
    
    * Remove caching as we build no charms here
    
    * Rise MySQL cluster init timeout to 15 mins on GH Runner
    
    * Add CI Integration test timetout to 30 mins

--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -2,15 +2,48 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Contains integration tests for the mysql-k8s-bundle."""
+"""Contains integration tests for the mysql-bundle."""
 
 import logging
 
+import pytest
 from pytest_operator.plugin import OpsTest
+
+from tests.integration.constants import APPLICATION_APP, MYSQL_APP, ROUTER_APP, TLS_APP
 
 logger = logging.getLogger(__name__)
 
 
-async def test_placeholder(ops_test: OpsTest) -> None:
-    """A placeholder for an integration test to be implemented."""
-    assert True
+@pytest.mark.abort_on_fail
+async def test_deploy_bundle(ops_test: OpsTest) -> None:
+    """Deploy bundle with app and release as a product."""
+    async with ops_test.fast_forward():
+        logger.info(f"Deploying test app {APPLICATION_APP}")
+        await ops_test.model.deploy(
+            APPLICATION_APP,
+            channel="edge",
+            application_name=APPLICATION_APP,
+            series="focal",
+            num_units=3,
+        )
+
+        logger.info(f"Deploying bundle: {MYSQL_APP}, {ROUTER_APP} and {TLS_APP}")
+        await ops_test.model.deploy("./releases/latest/mysql-bundle.yaml")
+
+        logger.info(f"Waiting for {MYSQL_APP} up and running...")
+        await ops_test.model.wait_for_idle(
+            apps=[MYSQL_APP],
+            status="active",
+            raise_on_blocked=True,
+            timeout=15 * 60,
+        )
+
+        logger.info(f"Relate {APPLICATION_APP} with {ROUTER_APP}")
+        await ops_test.model.relate(f"{APPLICATION_APP}", f"{ROUTER_APP}")
+
+        logger.info("Waiting for all applications up and running...")
+        await ops_test.model.wait_for_idle(
+            apps=[MYSQL_APP, ROUTER_APP, TLS_APP, APPLICATION_APP],
+            status="active",
+            timeout=5 * 60,
+        )
