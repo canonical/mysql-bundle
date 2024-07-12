1db1bd5fec58026c6676cf7524dfb8d946acf24d
Shayan Patel <shayan.patel@canonical.com>
2024-07-12 16:18:41 +0000

    Misc fixes

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -8,14 +8,7 @@ from pytest_operator.plugin import OpsTest
 
 
 async def get_unit_ip(ops_test: OpsTest, unit_name: str) -> str:
-    """Wrapper for getting unit ip.
-
-    Args:
-        ops_test: The ops test object passed into every test case
-        unit_name: The name of the unit to get the address
-    Returns:
-        The (str) ip of the unit
-    """
+    """Wrapper for getting unit ip."""
     return_code, stdout, _ = await ops_test.juju("ssh", unit_name, "ip", "route")
 
     assert return_code == 0
@@ -31,14 +24,7 @@ async def get_unit_ip(ops_test: OpsTest, unit_name: str) -> str:
 
 
 async def get_credentials(unit: Unit, username: str) -> Dict:
-    """Helper to run an action to retrieve server config credentials.
-
-    Args:
-        unit: The juju unit on which to run the get-password action for server-config credentials
-
-    Returns:
-        A dictionary with the server config username and password
-    """
+    """Helper to run an action to retrieve credentials for a username."""
     action = await unit.run_action(action_name="get-password", username=username)
     result = await action.wait()
     return result.results
