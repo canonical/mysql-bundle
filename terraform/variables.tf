cdbb21c7ece9a55b3c45b88ee29cebde19c4c123
Paulo Machado <paulo.machado@canonical.com>
2024-10-15 12:08:33 -0300

    revision override and tls charm override

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,40 +21,40 @@ variable "arch" {
 }
 
 variable "mysql_charm_channel" {
-  description = "MySQL charm channel"
+  description = "MySQL K8s charm channel"
   type        = string
   default     = "8.0/stable"
 }
 
 
 variable "mysql_charm_units" {
-  description = "MySQL charm units number"
+  description = "MySQL K8s charm units number"
   type        = number
   default     = 3
 }
 
 variable "mysql_backup_endpoint" {
-  description = "MySQL backup bucket endpoint"
+  description = "MySQL K8s backup bucket endpoint"
   type        = string
 }
 
 variable "mysql_backup_bucket_name" {
-  description = "MySQL backup bucket name"
+  description = "MySQL K8s backup bucket name"
   type        = string
 }
 
 variable "mysql_backup_region" {
-  description = "MySQL backup bucket region"
+  description = "MySQL K8s backup bucket region"
   type        = string
 }
 
 variable "mysql_backup_access_key" {
-  description = "MySQL backup bucket access key"
+  description = "MySQL K8s backup bucket access key"
   type        = string
 }
 
 variable "mysql_backup_secret_key" {
-  description = "MySQL backup bucket secret key"
+  description = "MySQL K8s backup bucket secret key"
   type        = string
   sensitive   = true
 }
@@ -114,9 +114,51 @@ variable "s3_integrator_charm_channel" {
 }
 
 
-variable "self_signed_certificates_charm_channel" {
+variable "certificates_charm_channel" {
   description = "Self Signed Certificates Operator charm channel"
   type        = string
   default     = "latest/stable"
+}
+
+variable "mysql_charm_revision" {
+  description = "MySQL charm revision override"
+  type        = number
+  default     = null
+}
+
+variable "mysql_router_charm_revision" {
+  description = "MySQL router charm revision override"
+  type        = number
+  default     = null
+}
+
+variable "s3_integrator_charm_revision" {
+  description = "s3_integrator charm revision override"
+  type        = number
+  default     = null
+}
+
+variable "certificates_charm_revision" {
+  description = "Certificates charm revision override"
+  type        = number
+  default     = null
+}
+
+variable "certificates_charm_name" {
+  description = "Certificates charm name"
+  type        = string
+  default     = "self-signed-certificates"
+}
+
+variable "certificates_charm_config" {
+  description = "Certificates charm configuration"
+  type        = map(string)
+  default     = { ca-common-name = "MySQL CA" }
+}
+
+variable "data_integrator_charm_revision" {
+  description = "data-integrator charm revision override"
+  type        = number
+  default     = null
 }
 
